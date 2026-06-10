302ea4c41849baeb9096e9b166ec2ba6a7275c0e
Alysome <alywillson32@gmail.com>
2026-06-10 18:47:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,78 @@ setTimeout(() => {
     setInterval(nextSlide, 5000);
 
 }, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const audio = document.getElementById("bgMusic");
+const playBtn = document.getElementById("playBtn");
+const seekBar = document.getElementById("seekBar");
+const volumeBar = document.getElementById("volumeBar");
+const time = document.getElementById("time");
+
+audio.volume = 0.5;
+
+/* autoplay po prvem kliku na stran */
+document.addEventListener("click", () => {
+
+    audio.play();
+
+    playBtn.innerHTML = "❚❚";
+
+}, { once:true });
+
+playBtn.onclick = () => {
+
+    if(audio.paused){
+
+        audio.play();
+        playBtn.innerHTML = "❚❚";
+
+    }else{
+
+        audio.pause();
+        playBtn.innerHTML = "▶";
+    }
+};
+
+volumeBar.oninput = () => {
+    audio.volume = volumeBar.value;
+};
+
+audio.addEventListener("loadedmetadata", () => {
+
+    seekBar.max = audio.duration;
+});
+
+audio.addEventListener("timeupdate", () => {
+
+    seekBar.value = audio.currentTime;
+
+    const minutes =
+    Math.floor(audio.currentTime / 60);
+
+    const seconds =
+    Math.floor(audio.currentTime % 60)
+    .toString()
+    .padStart(2,"0");
+
+    time.textContent =
+    `${minutes}:${seconds}`;
+});
+
+seekBar.oninput = () => {
+
+    audio.currentTime = seekBar.value;
+};
